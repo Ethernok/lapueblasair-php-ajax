a509833de3bbfaca12cec61b0da1ee50a56a945d
Shr00m5 <egomezlopez.ieslapuebla@gmail.com>
2021-04-20 19:38:48 +0200

    Eliminacion de archivos innecesarios

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conexion.php';
-$db = new db();
-    session_start();
-    session_destroy();
-    $db->close();
-    header('Location: login.php');
-
-?>
--- procesarLogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    include_once 'conexion.php';
-    $db = new db();
-    $user = $_POST['username'];
-    $pass = $_POST['pass'];
-    $db->procesarLogin($user, $pass);
-    
-?>
